5c3cd2ac4e0f394251a1942938a02d2c4f3701b2
Toney Mutinda <toneymutinda@gmail.com>
2017-09-07 23:14:52 +0300

    added link to welcome posts

--- resources/views/frontend/partials/welcome/_from_the_blog.blade.php
+++ resources/views/frontend/partials/welcome/_from_the_blog.blade.php
@@ -10,13 +10,13 @@
                     <div class="col-md-6 col-sm-6">
                         <div class="blog-img-text res">
                             <div class="blog-img">
-                                <a href=""><img src="{{ asset('images/posts/' . $post->image) }}" alt="" style="width: 300px; height: 260px;"></a>
+                                <a href="{{ route('blog.single', $post->slug) }}"><img src="{{ asset('images/posts/' . $post->image) }}" alt="" style="width: 300px; height: 260px;"></a>
                             </div>
                             <div class="blog-text">
                                 <span>{{ date('M j, Y', strtotime($post->created_at)) }}</span>
                                 <h4><a href="text-animator.html#">{{ $post->title }}</a></h4>
                                 <p>{{ substr(strip_tags($post->body), 0, 80) }}{{ strlen(strip_tags($post->body)) > 80 ? "..." : "" }}</p>
-                                <a href="">Read More <i class="ion-arrow-right-c"></i></a>
+                                <a href="{{ route('blog.single', $post->slug) }}">Read More <i class="ion-arrow-right-c"></i></a>
                             </div>
                         </div>
                     </div>
